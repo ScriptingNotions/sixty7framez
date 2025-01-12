c73bc050bcdf636434aa9fa92e9b4311e097f596
devantonio <antonio.ae91@gmail.com>
2025-01-11 20:44:38 -0500

    added mobile navigation content

---
+++ src/Views/components/mobile-nav-menu.component.php
@@ -0,0 +1,11 @@
+<div class="mobile-nav-menu-container">
+    <ul class="mobile-nav-menu" >
+        <li>Home</li>
+        <li>Photo booths</li>
+        <li>The process</li>
+        <li>Packages</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+    <button>Book Now</button>
+</div>
