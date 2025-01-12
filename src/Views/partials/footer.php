73b2d37c08d75a3ddea0815713129894e3f6d9a1
devantonio <antonio.ae91@gmail.com>
2025-01-11 20:30:58 -0500

    added footer section

--- src/Views/partials/footer.php
+++ src/Views/partials/footer.php
@@ -1,4 +1,25 @@
-        </section>
+        <footer>
+            <div class="footer-content">
+                <div class="footer-content-1">
+                    <img src="" alt="brand logo">
+                </div >
+                <div class="footer-content-2">
+                    <ul>
+                        <li><img src="" alt="twitter icon"></li>
+                        <li><img src="" alt="facebook icon"></li>
+                        <li><img src="" alt="instagram icon"></li>
+                    </ul>
+                </div>
+                <div class="footer-content-3">
+                    <p>2025 Sixty7Framez Photo Booth Events. All right reserved.</p>
+                    <ul>
+                        <li>Terms of service</li>
+                        <li>Privacy policy</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+
     </main>
     <script src="<?= $this->getScript("app"); ?>" type="module"></script>
 </body>
