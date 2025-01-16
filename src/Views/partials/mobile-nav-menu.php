586f8507834b737d3eeb95ad6d8a9dd4c94f8cef
devantonio <antonio.ae91@gmail.com>
2025-01-15 22:34:44 -0500

    added link to booking button

--- src/Views/partials/mobile-nav-menu.php
+++ src/Views/partials/mobile-nav-menu.php
@@ -2,11 +2,11 @@
     <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/process">The process</a></li>
-        <li><a href="/packages">Packages</a></li>
+        <li><a href="/#packages">Packages</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/contact">Contact</a></li>
     </ul>
     <div>
-        <button class="mobile-book-now-button">Book now</button>
+        <button class="mobile-book-now-button"><a href="/booking">Book now</a></button>
     </div>
 </div>
