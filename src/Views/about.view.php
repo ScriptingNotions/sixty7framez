a7e4a8257b87d8f7fa5469bc5be83327f299b581
devantonio <antonio.ae91@gmail.com>
2025-01-15 22:38:04 -0500

    updated about page

--- src/Views/about.view.php
+++ src/Views/about.view.php
@@ -1,18 +1,20 @@
 <?= $this->partial("header"); ?>
 
+<?= $this->partial("nav"); ?>
+
 <section class="about-section-1">
-    <h2>About Sixty7Framez.</h2>
+    <h2>About Sixty7Framez <span>.</span></h2>
     <p>Sixty7 Framez Photo Booth Events was born out of a passion for photography, event planning and a love for capturing life’s most joyful moments. We noticed that while everyone loves taking photos, there’s something uniquely special about the experience of a photo booth. It’s more than just snapping a picture—it’s about bringing people together, sparking creativity, and creating memories that last long after the event is over.</p>
     <p>We set out to create a photo booth experience that’s not only fun and engaging but also seamlessly blends into the modern, tech-savvy world we live in. Our booths are designed with the latest technology and customizable features to match the style and vibe of any event.</p>
 </section>
 
 <section class="about-section-2">
-    <h2>Our Mission.</h2>
+    <h2>Our Mission <span>.</span></h2>
     <p>Our mission is simple: To make your event unforgettable. We believe that every celebration deserves to be remembered in the best possible way, and we strive to provide an experience that’s both entertaining and elegant. From the moment your guests step into our photo booth to the instant they receive their prints or digital copies, we’re committed to delivering exceptional quality and service.</p>
 </section>
 
 <section class="about-section-3">
-    <h2>What Sets Us Apart.</h2>
+    <h2>What Sets Us Apart <span>.</span></h2>
     <p>At Sixty7 Framez Photo Booth Events, we pride ourselves on offering more than just a photo booth. We offer a fully customizable experience tailored to your event’s unique needs and style. Here’s what makes us stand out:</p>
     <ul>
         <li><p>State-of-the-Art Technology: Our photo booths are equipped with high-resolution cameras, professional lighting, and user-friendly touchscreens to ensure that every shot is picture-perfect.</p></li>
@@ -23,7 +25,7 @@
 </section>
 
 <section class="about-section-4">
-    <h2>Join the Fun.</h2>
+    <h2>Join the Fun <span>.</span></h2>
     <p>We’ve had the pleasure of being part of countless celebrations, and we can’t wait to be a part of yours. Whether it’s an intimate gathering or a large-scale event, Sixty7 Framez Photo Booth Events is here to help you capture the magic of the moment.
     Thank you for considering us for your special occasion. We look forward to bringing joy, laughter, and lasting memories to your next event!</p>
 </section>
