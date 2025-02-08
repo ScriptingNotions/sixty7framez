0633197059945beb4a12052a0b3ec965134b274d
devantonio <antonio.ae91@gmail.com>
2025-02-07 21:02:32 -0500

    addded timezome

--- src/App.php
+++ src/App.php
@@ -8,6 +8,8 @@ class App
     // Like main
     public function run(): void
     {
+        date_default_timezone_set('America/New_York');
+        
         $routes = new Routes();
 
         $routes->registerRoutes();
