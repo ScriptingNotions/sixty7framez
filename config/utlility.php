1e887de7341a08df2ce742ecbff089305288d04e
devantonio <antonio.ae91@gmail.com>
2025-02-07 21:01:26 -0500

    added uid function

--- config/utlility.php
+++ config/utlility.php
@@ -104,3 +104,15 @@ function isValidObjectId($id) {
     }
     
 }
+
+function uid() {
+    $uid = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+        mt_rand(0, 0xffff),
+        mt_rand(0, 0x0fff) | 0x4000,
+        mt_rand(0, 0x3fff) | 0x8000,
+        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+    );
+
+    return $uid;
+}
